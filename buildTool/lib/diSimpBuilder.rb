8d9416a1143f53ad96a9b60515d8a21556bf9f75
Stephen Gaito <stephen@perceptisys.co.uk>
2015-08-27 17:28:29 +0100

    added proof environment to disitt.dtx for use in TexStudio cwl files; added release code for releasing style and cwl files

--- buildTool/lib/diSimpBuilder.rb
+++ buildTool/lib/diSimpBuilder.rb
@@ -5,6 +5,7 @@ require 'pp'
 require 'fileutils'
 require 'diSimpBuilder/configuration'
 require 'diSimpBuilder/commands'
+require 'diSimpBuilder/diSimplexRelease'
 
 module DiSimpBuilder
   VERSION = '0.1'
