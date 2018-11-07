56130fb2a12734b828c1ecfa6202a45870f404d2
Stephen Gaito <stephen@perceptisys.co.uk>
2018-11-07 17:17:28 +0000

    initial sketch of relativeComponents

--- context/t-diSimp/tex/context/third/diSimp/t-diSimp.lua
+++ context/t-diSimp/tex/context/third/diSimp/t-diSimp.lua
@@ -27,7 +27,55 @@ local toStr   = tostring
 
 interfaces.writestatus('diSimp', "loaded diSimp macros")
 
--- from file: documentSetup.tex after line: 350
+-- from file: documentSetup.tex after line: 300
+
+local relativePaths = {}
+local pathSeparator = package.config:sub(1, 1)
+
+local function pushRelativePath(aRelativePath)
+  local pp = require 'pl.pretty'
+  local aRelativePathDir =
+    aRelativePath:gsub('[^'..pathSeparator..']+$', '')
+  tInsert(relativePaths, aRelativePathDir)
+end
+
+-- repeat after me... this WILL break!!!
+--
+-- the use of environments.arguments.fulljobname
+-- was infered from grep'ing the experimental distribution
+-- for fulljobname and finding it defined in the
+-- the environment table.
+-- (defined in core-sys.lua)
+--
+pushRelativePath(environment.arguments.fulljobname)
+
+local function popRelativePath()
+  pp = require 'pl.pretty'
+  texio.write_nl('     relativePaths: ['..pp.write(relativePaths)..']')
+  tRemove(relativePaths)
+  texio.write_nl('     relativePaths: ['..pp.write(relativePaths)..']')
+end
+
+diSimp.popRelativePath = popRelativePath
+
+local function relativeComponent(componentType, componentPath)
+  local thisComponentPath = tConcat(relativePaths)..componentPath
+  thisComponentPath = file.collapsepath(thisComponentPath, true)
+  texio.write_nl('relativeComponent: ['..componentType..']')
+  texio.write_nl('relativeComponent: ['..componentPath..']')
+  texio.write_nl('relativeComponent: ['..thisComponentPath..']')
+  pushRelativePath(componentPath)
+  pp = require 'pl.pretty'
+  texio.write_nl('     relativePaths: ['..pp.write(relativePaths)..']')
+  tex.print({
+    '\\'..componentType..' '..thisComponentPath,
+    '\\popRelativePath'
+  })
+end
+
+diSimp.relativeComponent = relativeComponent
+
+-- from file: documentSetup.tex after line: 450
 
 -- repeat after me... this WILL break!!!
 --
