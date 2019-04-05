a74a5b6b4550b21ee7f06c0482a5386cab763b59
Stephen Gaito <stephen@perceptisys.co.uk>
2019-04-05 08:48:47 +0100

    added diSimp licenses

--- context/t-diSimp/tex/context/third/diSimp/t-diSimp.lua
+++ context/t-diSimp/tex/context/third/diSimp/t-diSimp.lua
@@ -2,6 +2,32 @@
 
 -- from file: preamble.tex after line: 50
 
+-- Copyright 2019 PerceptiSys Ltd (Stephen Gaito)
+--
+-- Permission is hereby granted, free of charge, to any person
+-- obtaining a copy of this software and associated documentation
+-- files (the "Software"), to deal in the Software without
+-- restriction, including without limitation the rights to use,
+-- copy, modify, merge, publish, distribute, sublicense, and/or sell
+-- copies of the Software, and to permit persons to whom the
+-- Software is furnished to do so, subject to the following
+-- conditions:
+--
+--    The above copyright notice and this permission notice shall
+--    be included in all copies or substantial portions of the
+--    Software.
+--
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+-- EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+-- OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+-- NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+-- HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+-- WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+-- FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+-- OTHER DEALINGS IN THE SOFTWARE.
+
+-- from file: preamble.tex after line: 0
+
 -- This is the lua code associated with t-diSimp.mkiv
 
 if not modules then modules = { } end modules ['t-diSimp'] = {
@@ -161,7 +187,7 @@ end
 
 diSimp.stopDiSimpComponent = stopDiSimpComponent
 
--- from file: documentSetup.tex after line: 600
+-- from file: documentSetup.tex after line: 550
 
 -- repeat after me... this WILL break!!!
 --
